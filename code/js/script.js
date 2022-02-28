970c580b261f3eceb2b69b81839b2759fff24224
lindylouwerse <98382885+lindylouwerse@users.noreply.github.com>
2022-02-28 20:55:12 +0100

    Add files via upload

---
+++ code/js/script.js
@@ -0,0 +1,7 @@
+//For the burger menu (Javascript)
+//const navbarToggle = () => {
+//const nav = document.querySelector('.mainnav'); 
+//nav.classList.toggle('toggleShow');
+//} 
+//document.querySelector('.fa-bars').addEventListener('click', navbarToggle);
+
